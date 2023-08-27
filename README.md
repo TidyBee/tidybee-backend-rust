bbe2a5a4eb55aa68dbdb501da7bbfd1b4bc81b3f
majent4 <99695392+majent4@users.noreply.github.com>
2023-08-27 11:50:12 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# tidybee-backend-rust
+Watch for changes in directories and recursively list directories.
+
+## Tools
+- rust: the language
+- cargo: the language package manager
+- nc: netcat for testing purposes only
+
+## Before you run
+Launch a server listening on port 8080:
+```
+# this is the way I did it on macOS
+while true; do nc -l 8080; done
+```
+
+## Build & Run
+As usual, you have two ways to build and run a program:
+```
+# build
+cargo build
+# run
+./target/debug/tidybee <commands>
+```
+Shorter:
+```
+# build & run
+cargo run <commands>
+```
+
+## Mandatory commands
+```
+-l, --list <dir>    # recursively list the given directories, send it to localhost:8080 (json) and exit
+-w, --watch <dir>   # whatch for changes in the given directories and send it to localhost:8080 (json)
+```
+
+## Optional commands
+Optional commands are parsed in the configuration module, but not implemented!
+```
+-e, --extension <exts>  # file extensions to list/watch (default: all)
+-t, --type <types>      # file types to list/watch (default: all)
+-r, --receive <addr>    # receive json data from this address (file manager stuff)
+-s, --send <addr>       # send json data to this address (send file list/events)
+```
+
+## Run the test suite
+```
+cargo test
+```
---
+++ src/main.rs
@@ -0,0 +1,74 @@
+mod listing;
+mod options;
+mod watcher;
+
+use std::process;
+use std::thread;
+use tokio::io::AsyncWriteExt;
+use tokio::net::TcpStream;
+
+#[tokio::main]
+async fn main() {
+    let mut stream: TcpStream = match TcpStream::connect("localhost:8080").await {
+        Ok(stream) => stream,
+        Err(e) => {
+            eprintln!("tidybee: error: {}", e);
+            process::exit(1);
+        }
+    };
+
+    if let Err(e) = send_this(&mut stream, "hiiii").await {
+        eprintln!("tidybee: error: {}", e);
+    }
+
+    let options: Result<options::Options, options::OptionsError> = options::get_options();
+
+    match options {
+        Ok(opts) => {
+            if let Some(directories) = opts.directories_list_args {
+                match listing::list_directories(directories) {
+                    Ok(files) => {
+                        let json_data: String = serde_json::to_string_pretty(&files).unwrap();
+                        if let Err(e) = send_this(&mut stream, &json_data).await {
+                            eprintln!("tidybee: error: {}", e);
+                        }
+                    }
+                    Err(error) => {
+                        eprintln!("tidybee: error: {}", error);
+                    }
+                }
+            } else if let Some(directories) = opts.directories_watch_args {
+                let (sender, receiver) = crossbeam_channel::unbounded();
+                let watch_directories_thread: thread::JoinHandle<()> = thread::spawn(move || {
+                    watcher::watch_directories(
+                        directories.clone(),
+                        opts.file_extensions_args.clone(),
+                        opts.file_types_args.clone(),
+                        sender,
+                    );
+                });
+                for event in receiver {
+                    println!("new event: {event:?}");
+                }
+                watch_directories_thread.join().unwrap();
+            }
+        }
+        Err(error) => {
+            options::print_option_error(error);
+            process::exit(1);
+        }
+    }
+
+    if let Err(e) = send_this(&mut stream, "byeee").await {
+        eprintln!("tidybee: error: {}", e);
+    }
+}
+
+async fn send_this(
+    stream: &mut TcpStream,
+    message: &str,
+) -> Result<(), Box<dyn std::error::Error>> {
+    stream.write_all(message.as_bytes()).await?;
+    stream.flush().await?;
+    Ok(())
+}
